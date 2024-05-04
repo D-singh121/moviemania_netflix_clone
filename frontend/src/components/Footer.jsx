9348ad989604ee94fcc2111079c48b5ea451d415
Devesh_singh <choudharydevesh121@gmail.com>
2024-05-04 17:15:31 +0530

    Done with this project

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
 	return (
 		<div className="w-full bg-black text-white pt-8 bottom-0 px-5 opacity-95 ">
-			<h2 className="pl-28 opacity-60 text-xl">Questions? call 000-216-131-142</h2>
+			<h2 className="pl-28 opacity-60 text-xl mt-5">Questions? call 000-216-131-142</h2>
 			<div className="flex justify-around opacity-60 underline pb-16 ">
 				<div className="py-10 cursor-pointer">
 					<div className="py-6">FAQ</div>

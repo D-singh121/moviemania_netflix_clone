6cc773ac2bb3caa1a0f99f78ead95527a0a75151
Devesh_singh <choudharydevesh121@gmail.com>
2024-05-08 21:18:14 +0530

    changed frontend origin

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cookieParser());
 const corsOptions = {
-	origin: 'http://localhost:5173',
+	origin: process.env.FRONTEND_URL,
 	method: ["GET", "POST", "DELETE", "PUT", "PATCH"],
 	credentials: true
 }

eb30b277262788bb5e2155e24134d082475a6e2d
Devesh_singh <choudharydevesh121@gmail.com>
2024-06-25 14:40:03 +0530

    modified some codes in db

--- backend/src/database/dbConnection.js
+++ backend/src/database/dbConnection.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const MONGO_URI = process.env.MONGO_URI
 
 const dbConnection = async () => {
 	try {
+		// const connectionInstance = await mongoose.connect(`${MONGO_URI}`)
 		const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`)
 		console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
 	} catch (error) {

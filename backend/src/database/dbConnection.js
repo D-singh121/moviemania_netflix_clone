04ddee0b6daa76265d5c4891d2312f2d8a034e0e
Devesh_singh <choudharydevesh121@gmail.com>
2024-06-26 03:34:31 +0530

    all working till docker-compose file

--- backend/src/database/dbConnection.js
+++ backend/src/database/dbConnection.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
-// const MONGO_URI = process.env.MONGO_URI
+const MONGO_URL = process.env.MONGO_URL
 
 const dbConnection = async () => {
 	try {
-		// const connectionInstance = await mongoose.connect(`${MONGO_URI}`)
-		const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`)
+		const connectionInstance = await mongoose.connect(MONGO_URL)
+		// const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`)
 		console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
 	} catch (error) {
 		console.log(`MONGODB connection FAILED : ${error}`);
